d57a576d3c6a2d83b10a8614b1958466606c38ad
baekunyong <turbo6040@gmail.com>
2020-07-23 17:41:48 +0900

    myrecipe 테이블, 시퀀스 추가함/ test.bbs.dao, test.bbs.dto 패키지 삭제함

--- Project/WebContent/WEB-INF/table.sql
+++ Project/WebContent/WEB-INF/table.sql
@@ -20,3 +20,18 @@ CREATE TABLE bbs_cafe (
 
 CREATE SEQUENCE bbs_cafe_seq;  -- bbs 테이블의 num에서 사용할 시퀀스
 
+
+-- myrecipe 폴더에서 쓸 table --
+CREATE TABLE my_recipe(
+	num NUMBER PRIMARY KEY, -- 글 번호
+	title VARCHAR2(100) NOT NULL, -- 글 제목
+	subTitle VARCHAR2(150), -- 해당 요리에 대한 간략한 설명
+	content CLOB, -- 글의 전체 내용
+	writer VARCHAR2(50), -- 작성자
+	regdate DATE, -- 작성일
+	imagePath VARCHAR2(200), -- 대표 이미지 경로
+	showImage VARCHAR2(150) -- 대표 이미지 실제 이름 
+);
+
+CREATE SEQUENCE my_recipe_seq;  -- my_recipe 테이블의 num에서 사용할 시퀀스
+
